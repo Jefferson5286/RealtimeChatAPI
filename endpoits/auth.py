674cbc1a7a00bb05d55a0978228d99e55d670181
Jefferson5286 <jeffersonlima5286@gmail.com>
2024-06-02 12:27:10 -0300

    Especificação OpenAPI implementada; Pequenas correções e melhorias

--- endpoits/auth.py
+++ endpoits/auth.py
@@ -4,6 +4,7 @@ from services.auth import *
 from exceptions.user import *
 from exceptions.jwt import *
 from schemas.auth import *
+from openapi import openapi
 
 from fastapi import APIRouter, HTTPException, Request, Response
 from fastapi.responses import JSONResponse
@@ -11,8 +12,12 @@ from fastapi.responses import JSONResponse
 
 router = APIRouter(prefix='/auth')
 
+LOGIN_SCHEMA = openapi.get_responses('post', '/auth/login')
+REGISTER_SCHEMA = openapi.get_responses('post', '/auth/register')
+REFRESH_SCHEMA = openapi.get_responses('get', '/auth/refresh')
 
-@router.post('/register')
+
+@router.post('/register', responses=REGISTER_SCHEMA)
 async def register(data: UserRegisterSchema) -> Response:
     try:
         await to_thread(create_user, data)
@@ -23,7 +28,7 @@ async def register(data: UserRegisterSchema) -> Response:
         raise HTTPException(409, 'E-mail already registered!')
 
 
-@router.post('/login')
+@router.post('/login', responses=LOGIN_SCHEMA)
 async def login(data: UserLoginSchema) -> JSONResponse:
     try:
         token = await to_thread(login_account, data)
@@ -34,7 +39,7 @@ async def login(data: UserLoginSchema) -> JSONResponse:
         raise HTTPException(401, 'Unauthorized!')
 
 
-@router.get('/refresh')
+@router.get('/refresh', responses=REFRESH_SCHEMA)
 async def refresh(request: Request) -> JSONResponse:
     try:
         token = await to_thread(refresh_token, request.headers['Authorization'])
--- services/account.py
+++ services/account.py
@@ -1,5 +1,11 @@
 from typing import Dict
 
+from utils.security import Token, JsonWebToken
+from firestore import client
+
 
 def get_account_data_from_token(token: str) -> Dict[str, str]:
-    pass
+    collection = client.collection('users')
+    token_data: Token = JsonWebToken.decode(token)
+
+
