8f82370bf647309ded41b70afc3f4115a897d2e9
Jefferson5286 <jeffersonlima5286@gmail.com>
2024-06-03 20:30:05 -0300

    Sistema de chat foi Reconstruido

--- endpoits/chat.py
+++ endpoits/chat.py
@@ -1,7 +1,5 @@
 from typing import Dict
-
-from fastapi import WebSocket, APIRouter, HTTPException
-
+from fastapi import WebSocket, APIRouter
 
 router = APIRouter(prefix='/chat')
 connections: Dict[str, WebSocket] = dict()
@@ -10,24 +8,22 @@ connections: Dict[str, WebSocket] = dict()
 async def websocket_connection_manager(websocket: WebSocket, sender: str):
     connections[sender] = websocket
 
-    while True:
-        content = await websocket.receive_json()
+    try:
+        while True:
+            content = await websocket.receive_json()
 
-        message = content['message']
-        target = content['target']
+            message = content['message']
+            target = content['target']
 
-        if target in connections.keys():
-            target_socket = connections[target]
+            if target in connections.keys():
+                target_socket = connections[target]
+                await target_socket.send_json({'message': message, 'sender': sender})
 
-            await target_socket.send_json({'message': message, 'sender': sender})
+    except Exception as e:
+        del connections[sender]
+        print(e)
 
 
 @router.websocket('/{token}')
 async def chat_of_target(websocket: WebSocket, token: str):
-    try:
-        await websocket_connection_manager(websocket, token)
-
-    except Exception as e:
-        await websocket.close()
-        print(e)
-        raise HTTPException(500, 'Internal server Error.')
+    await websocket_connection_manager(websocket, token)
